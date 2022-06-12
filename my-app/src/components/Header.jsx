aec5e38b5750b7e1da2cf68832b53a52e3110d32
Migoyan Edgar <migoyane@gmail.com>
2022-06-12 19:51:38 +0400

    Sample comit

---
+++ my-app/src/components/Header.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+
+const Header = () => {
+    return <header className='header'>
+        <img src='https://api.freelogodesign.org/assets/thumb/logo/15101367_400.png?t=637873803620000000'></img>
+    </header>
+}
+export default Header;
